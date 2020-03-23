6e803c6e1f8c93a3d36c098a125f659e716c5eed
ParkYongHwan <pypy8171@gmail.com>
2020-03-23 21:12:37 +0900

    seperation client & server

--- SPServer/SPServer.cpp
+++ SPServer/SPServer.cpp
@@ -22,14 +22,35 @@ typedef struct PlayerSockInfo
 
 }PLAYERSOCKINFO;
 
+typedef struct SockInfo
+{
+	SOCKET	socket;
+	char	packet_buf[MAX_BUFFER];
+
+	int		recvbytes;
+	int		sendbytes;
+
+}SOCKINFO;
+
 PLAYERSOCKINFO clients[MAX_USER];
 
 // 오류 출력 함수
 void err_quit(char* msg);
 void err_display(char* msg);
-void process_packet(char id, char* buf);
+int recvn(SOCKET s, char* buf, int len, int flags);
+void RemoveSocketInfo(int nIndex);
+void process_packet(sc_packet_pos pos);
 void send_packet(int key, char* packet);
-void send_pos_packet(char to, char obj);
+void send_pos_packet(/*char to, char obj*/);
+bool AddSocketInfo(SOCKET sock);
+
+int nTotalSockets = 0;
+SOCKINFO* SocketInfoArray[FD_SETSIZE];
+
+sc_packet_pos pos;
+
+int g_x, g_y = 0;
+bool g_connect = false;
 
 // 소켓 함수 오류 출력 후 종료
 void err_quit(char* msg)
@@ -58,6 +79,45 @@ void err_display(char* msg)
 	LocalFree(lpMsgBuf);
 }
 
+int recvn(SOCKET s, char* buf, int len, int flags)      // 고정길이 데이터를 읽기 위해 사용자 정의함수 recvn() 함수 사용 ~62라인까지 - 4장에서 사용
+{
+	int received;
+	char* ptr = buf;
+	int left = len;
+
+	while (left > 0) {
+		received = recv(s, ptr, left, flags);
+		if (received == SOCKET_ERROR)
+			return SOCKET_ERROR;
+		else if (received == 0)
+			break;
+		left -= received;
+		ptr += received;
+	}
+
+	return (len - left);
+}
+
+void RemoveSocketInfo(int nIndex)
+{
+	SOCKINFO* ptr = SocketInfoArray[nIndex];
+
+	// 클라이언트 정보 얻기
+	SOCKADDR_IN clientaddr;
+	int addrlen = sizeof(clientaddr);
+	getpeername(ptr->socket, (SOCKADDR*)&clientaddr, &addrlen);
+	printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n",
+		inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+	closesocket(ptr->socket);
+	delete ptr;
+
+	if (nIndex != (nTotalSockets - 1))
+		SocketInfoArray[nIndex] = SocketInfoArray[nTotalSockets - 1];
+
+	--nTotalSockets;
+}
+
 void send_packet(int key, char* packet)
 {
 	SOCKET client_s = clients[key].socket;
@@ -80,56 +140,79 @@ void send_packet(int key, char* packet)
 	//}
 }
 
-void send_pos_packet(char to, char obj)
+void send_pos_packet(/*char to, char obj*/)
 {
 	sc_packet_pos packet;
-	packet.id = obj;
+	//packet.id = obj;
 	packet.size = sizeof(packet);
 	packet.type = SC_POS;
-	packet.x = clients[obj].x;
-	packet.y = clients[obj].y;
-	send_packet(to, reinterpret_cast<char*>(&packet));
+	packet.x = g_x;
+	packet.y = g_y;
+	//send_packet(to, reinterpret_cast<char*>(&packet));
 }
 
-void process_packet(char id, char* buf)
+void process_packet(sc_packet_pos pos)
 {
-	cs_packet_up* packet = reinterpret_cast<cs_packet_up*>(buf);
+	int iType = pos.type;
 
-	char x = clients[id].x;
-	char y = clients[id].y;
-	switch (packet->type) {
+	if (iType < 1 || iType >5)
+		return;
+
+	switch (iType) {
 	case CS_UP:
-		y-=BOARD_GAP;
-		if (y < 0)
-			y = 0;
+		if (pos.y > 0)
+		{
+			pos.y -= BOARD_GAP;
+		}
 		break;
 	case CS_DOWN:
-		y += BOARD_GAP;
-		if (y >= WORLD_HEIGHT) 
-			y = WORLD_HEIGHT - BOARD_GAP;
+		if (pos.y < BOARD_GAP * 7)
+		{
+			pos.y += BOARD_GAP;
+		}
 		break;
 	case CS_LEFT: 
-		if (0 < x) 
-			x -= BOARD_GAP; 
+		if (0 < pos.x)
+		{
+			pos.x -= BOARD_GAP;
+		}
 		break;
 	case CS_RIGHT: 
-		if ((WORLD_WIDTH - BOARD_GAP) > x)
-			x += BOARD_GAP;
+		if ((BOARD_GAP * 7) > pos.x)
+		{
+			pos.x += BOARD_GAP;
+		}
 		break;
 	default:
 		cout << "Unknown Packet Type Error\n";
-		while (true);
+		break;
 	}
-	clients[id].x = x;
-	clients[id].y = y;
 
-	for (int i = 0; i < MAX_USER; ++i)
-	{
-		if (true == clients[i].connected)
-			send_pos_packet(i, id);
-	}
+	send(SocketInfoArray[0]->socket, (char*)&pos, sizeof(sc_packet_pos), 0);
+
+
 }
 
+bool AddSocketInfo(SOCKET sock)
+{
+	if (nTotalSockets >= FD_SETSIZE) {
+		printf("[오류] 소켓 정보를 추가할 수 없습니다!\n");
+		return FALSE;
+	}
+
+	SOCKINFO* ptr = new SOCKINFO;
+	if (ptr == NULL) {
+		printf("[오류] 메모리가 부족합니다!\n");
+		return FALSE;
+	}
+
+	ptr->socket = sock;
+	ptr->recvbytes = 0;
+	ptr->sendbytes = 0;
+	SocketInfoArray[nTotalSockets++] = ptr;
+
+	return TRUE;
+}
 
 DWORD WINAPI WorkerThread(LPVOID arg)
 {
@@ -144,7 +227,7 @@ DWORD WINAPI WorkerThread(LPVOID arg)
 		//초당 20번 전송
 		if (durationMs > 50)
 		{
-			//CalKeyInput(player, Curtime);
+			process_packet(pos);
 			last_time = chrono::high_resolution_clock::now();
 		}
 	}
@@ -152,61 +235,25 @@ DWORD WINAPI WorkerThread(LPVOID arg)
 	return 0;
 }
 
-void worker_thread()
+int main()
 {
-	while (true) {
-		//DWORD io_byte;
-		//ULONG key;
-		//OVER_EX* lpover_ex;
-		//BOOL is_error = GetQueuedCompletionStatus(g_iocp, &io_byte, (PULONG_PTR)&key,
-		//	reinterpret_cast<LPWSAOVERLAPPED*>(&lpover_ex), INFINITE);
+	int retval = 0;
 
-		//if (FALSE == is_error)
-		//	error_display("GQCS ", WSAGetLastError());
-		//if (0 == io_byte) disconnect(key);
+	HANDLE hThread = NULL;
+	hThread = CreateThread(NULL, 0, WorkerThread, NULL, 0, NULL);
 
-		//if (lpover_ex->is_recv) {
-		//	int rest_size = io_byte;
-		//	char* ptr = lpover_ex->messageBuffer;
-		//	char packet_size = 0;
-		//	if (0 < clients[key].prev_size) packet_size = clients[key].packet_buf[0];
-		//	while (rest_size > 0) {
-		//		if (0 == packet_size) packet_size = ptr[0];
-		//		int required = packet_size - clients[key].prev_size;
-		//		if (rest_size >= required) {
-		//			memcpy(clients[key].packet_buf + clients[key].prev_size, ptr, required);
-		//			process_packet(key, clients[key].packet_buf);
-		//			rest_size -= required;
-		//			ptr += required;
-		//			packet_size = 0;
-		//		}
-		//		else {
-		//			memcpy(clients[key].packet_buf + clients[key].prev_size,ptr, rest_size);
-		//			rest_size = 0;
-		//		}
-		//	}
-		//	do_recv(key);
-		//}
-		//else {
-		//	delete lpover_ex;
-		//}
-	}
-}
-
-void do_accept()
-{
 	WSADATA WSAData;
 	if (WSAStartup(MAKEWORD(2, 2), &WSAData) != 0)
 	{
 		cout << "Error - Can not load 'winsock.dll' file\n";
-		return;
+		return 0;
 	}
 
 	SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, 0); // 변경
 	if (listenSocket == INVALID_SOCKET)
 	{
 		cout << "Error - Invalid socket\n";
-		return;
+		return 0;
 	}
 
 	// 서버정보 객체설정 // 변경
@@ -216,16 +263,36 @@ void do_accept()
 	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serverAddr.sin_port = htons(SERVER_PORT);
 
-	// 2. 소켓설정
-	if (::bind(listenSocket, (struct sockaddr*) & serverAddr, sizeof(SOCKADDR_IN)) == SOCKET_ERROR)
-	{
-		cout << "Error - Fail bind\n";
-		// 6. 소켓종료
-		closesocket(listenSocket);
-		// Winsock End
-		WSACleanup();
-		return;
-	}
+	//// 2. 소켓설정
+	//if (::bind(listenSocket, (struct sockaddr*) & serverAddr, sizeof(SOCKADDR_IN)) == SOCKET_ERROR)
+	//{
+	//	cout << "Error - Fail bind\n";
+	//	// 6. 소켓종료
+	//	closesocket(listenSocket);
+	//	// Winsock End
+	//	WSACleanup();
+	//	return 0;
+	//}
+
+		// bind()
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVER_PORT);
+	retval = bind(listenSocket, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_quit("bind()");
+
+	// listen()
+	retval = listen(listenSocket, SOMAXCONN);
+	if (retval == SOCKET_ERROR) err_quit("listen()");
+
+	// 넌블로킹 소켓으로 전환
+	//********************************************************
+	u_long on = 1;
+	retval = ioctlsocket(listenSocket, FIONBIO, &on); // 교착상태 해결하기 위함, 여기서 쓸 필요는 없을듯
+	if (retval == SOCKET_ERROR) err_display("ioctlsocket()");
+
 
 	//// 3. 수신대기열생성
 	//if (listen(listenSocket, 5) == SOCKET_ERROR)
@@ -242,72 +309,84 @@ void do_accept()
 	int addrLen = sizeof(SOCKADDR_IN);
 	memset(&clientAddr, 0, addrLen);
 	SOCKET clientSocket;
-	//DWORD flags;
 
-	while (true)
-	{
-		clientSocket = accept(listenSocket, (struct sockaddr*) & clientAddr, &addrLen);
-		if (clientSocket == INVALID_SOCKET)
-		{
-			cout << "Error - Accept Failure\n";
-			return;
+	//********************************************************
+	FD_SET rset, wset;
+	//********************************************************
+	SOCKET client_sock;
+	SOCKADDR_IN clientaddr;
+	int addrlen, i;
+	int iClientID = -1;
+
+
+	while (1) {
+		// 소켓 셋 초기화
+		FD_ZERO(&rset);
+		FD_ZERO(&wset);
+		FD_SET(listenSocket, &rset); // fd_set은 관찰을 할 소켓들을 등록하는 변수
+		for (i = 0; i < nTotalSockets; i++) {
+			if (SocketInfoArray[i]->recvbytes > SocketInfoArray[i]->sendbytes)
+				FD_SET(SocketInfoArray[i]->socket, &wset);
+			else
+				FD_SET(SocketInfoArray[i]->socket, &rset);
 		}
 
-		//memset(&clients[0], NULL, sizeof(PLAYERSOCKINFO));
-		//clients[0].socket = clientSocket;
-		//clients[0].over.dataBuffer.len = MAX_BUFFER;
-		//clients[0].over.dataBuffer.buf = clients[clientSocket].over.messageBuffer;
-		//clients[0].over.is_recv = true;
-		clients[0].x = START_X;
-		clients[0].y = START_Y;
-		//flags = 0;
+		// select()
+		retval = select(0, &rset, &wset, NULL, NULL); // 대기 시간 0 // 블로킹이던 아니던 신경 안쓰고 여러 소켓 한 스레드로 처리
+		if (retval == SOCKET_ERROR) err_quit("select()");
 
-		//CreateIoCompletionPort(reinterpret_cast<HANDLE>(clientSocket), g_iocp, new_id, 0);
-		clients[0].connected = true;
+		// 소켓 셋 검사(1): 클라이언트 접속 수용
+		if (FD_ISSET(listenSocket, &rset)) { // 변화가 있는지를 확인
+			addrlen = sizeof(clientaddr);
+			client_sock = accept(listenSocket, (SOCKADDR*)&clientaddr, &addrlen);
+			if (client_sock == INVALID_SOCKET) {
+				err_display("accept()");
+			}
+			else {
+				u_long off = 0;
 
-		//send_login_ok_packet(new_id);
-		//for (int i = 0; i < MAX_USER; ++i) {
-		//	if (false == clients[i].connected) continue;
-		//	send_put_player_packet(i, new_id);
-		//}
-		//for (int i = 0; i < MAX_USER; ++i) {
-		//	if (false == clients[i].connected) continue;
-		//	if (i == new_id) continue;
-		//	send_put_player_packet(new_id, i);
-		//}
-		//do_recv(new_id);
+				retval = ioctlsocket(client_sock, FIONBIO, &off); // 인자를 off로 하는 이유가? // 교착상태 해결하기 위함, off 매개변수가 0이 아닐 경우 client_sock의 넌블럭킹(비동기) 모드를 활성화 함. off가 0일 경우는 넌블럭킹(비동기) 모드는 비활성화 됨. argp 매개변수는 unsigned long 값을 포인트 함.
+				if (retval == SOCKET_ERROR) err_display("ioctlsocket()");
+
+				printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
+					inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+				// 소켓 정보 추가
+				AddSocketInfo(client_sock);
+				g_connect = true;
+
+			}
+		}
+
+		// 소켓 셋 검사(2): 데이터 통신
+		for (int i = 0; i < nTotalSockets; i++) {
+			SOCKINFO* ptr = SocketInfoArray[i];
+			if (FD_ISSET(ptr->socket, &rset)) {
+
+				retval = recvn(ptr->socket, (char*)&pos, sizeof(sc_packet_pos), 0); // 키 받음
+				//std::cout << a << endl;
+				if (pos.type != 0)
+				{
+					std::cout << (int)pos.size << " , " << (int)pos.type<< " , " << (int)pos.x  << " , " <<(int)pos.y<< endl;
+				}
+				//send(ptr->sock, (char*)&gameinfo, sizeof(GAMEINFO), 0);
+
+				if (retval == SOCKET_ERROR) {
+					err_display("recvKeystate()");
+					RemoveSocketInfo(i);
+					continue;
+				}
+			}
+		}
 	}
 
-	// 6-2. 리슨 소켓종료
+	WaitForMultipleObjects(2, &hThread, TRUE, INFINITE);
+
 	closesocket(listenSocket);
-
-	// Winsock End
-	WSACleanup();
-
-	return;
-}
-
-int main()
-{
-	HANDLE hThread = NULL;
-
-	vector<thread> Worker_Threads;
-
-	//hThread = CreateThread(NULL, 0, WorkerThread, NULL, 0, NULL);
-
-	for (auto& client : clients) {
-		client.connected = false;
-	}
-
-	for (int i = 0; i < MAX_USER; ++i)
-	{
-		Worker_Threads.emplace_back(thread{ worker_thread });
-	}
-
-	thread accept_thread{ do_accept };
-
-	for (auto& thread : Worker_Threads)
-		thread.join();
-
 	CloseHandle(hThread);
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
 }
+
+// 로그인 부분 고치기 // 패킷 종류별로 나누기 // id 넣기 
--- SPServer/protocol.h
+++ SPServer/protocol.h
@@ -9,28 +9,36 @@
 #define START_Y 50
 
 #define BOARD_SIZE 600
-#define BOARD_GAP 75
+#define BOARD_GAP 5
 #define BOARD_GAPNUM 8
 
 #define WORLD_HEIGHT BOARD_SIZE
 #define WORLD_WIDTH BOARD_SIZE
 
+#define CS_NONE		0
 #define CS_UP		1
 #define CS_DOWN		2
 #define CS_LEFT		3
 #define CS_RIGHT	4
 
-#define SC_POS		1
+#define SC_POS		2
 
 struct sc_packet_pos
 {
 	char size;
 	char type;
-	char id;
-	char x, y;
+	//char id;
+	char x;
+	char y;
 };
 
 #pragma pack(push ,1)
+struct cs_packet_keyinput
+{
+	char size;
+	char type;
+};
+
 struct cs_packet_up
 {
 	char	size;
